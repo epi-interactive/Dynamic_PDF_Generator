36d29e5e55422a5378126c6515d56dcff55d7f86
EPI-interactive <info@epi-interactive.com>
2020-02-13 12:42:42 +1300

    Initial version.

---
+++ server.R
@@ -0,0 +1,36 @@
+library(knitr)
+
+
+if (is.null(suppressMessages(webshot:::find_phantom()))) { webshot::install_phantomjs() }
+
+function(input, output) {
+  plot_function <- function(n, mean) {
+    plot_ly(x = ~rnorm(n), type = "box") %>%
+      add_trace(x = ~rnorm(n, mean)) %>% 
+      layout(xaxis = list(visible=T, showgrid=F, showline=T, mirror=T,
+                          zeroline=F, zerolinecolor="#fff", title = paste0("norm(", n, ")")),
+             yaxis = list(visible=T, showgrid=F, showline=T, mirror=T,
+                          zeroline=F, zerolinecolor="#fff"))
+  }
+  
+  plot_example_1_chart <- reactive({plot_function(input$plot_example_1_n, input$plot_example_1_mean)})
+  
+  output$plot_example_1 <- renderPlotly({plot_example_1_chart()})
+  
+  output$downloadReport <- downloadHandler(
+    filename = "report.pdf",
+    
+    content = function(file) {
+      src <- normalizePath('report.Rnw')
+      
+      # temporarily switch to the temp dir, in case you do not have write
+      # permission to the current working directory
+      owd <- setwd(tempdir())
+      on.exit(setwd(owd))
+      file.copy(src, 'report.Rnw', overwrite = TRUE)
+      
+      out = knit2pdf('report.Rnw', clean = TRUE)
+      file.rename(out, file) 
+    }
+  )
+}
---
+++ ui.R
@@ -0,0 +1,15 @@
+library(plotly)
+
+fluidPage(
+  title = 'Download a PDF report',
+  sidebarLayout(
+    sidebarPanel(style="margin-top: 20px;",
+      numericInput("plot_example_1_n", "number of datasets to be simulated", 50, 25, 100, 5),
+      numericInput("plot_example_1_mean", "The mean of the dataset to be simulated", 5, 1, 10, 1),
+      downloadButton('downloadReport')
+    ),
+    mainPanel(
+      plotlyOutput("plot_example_1", height="auto")
+    )
+  )
+)
